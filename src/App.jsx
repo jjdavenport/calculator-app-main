5202dafd1085c1a9c3e304f1361b70c93dad441d
jjdavenport <jordanjamesdavenport@gmail.com>
2024-11-05 22:34:18 +0000

    increase padding on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-      <div className="flex h-full min-h-screen flex-col font-custom one:bg-mainBgTheme1 two:bg-mainBgTheme2 three:bg-mainBgTheme3 md:items-center">
-        <div className="flex w-screen flex-1 flex-col justify-center gap-6 p-4 text-custom font-bold transition-colors duration-700 ease-in-out md:max-w-screen-sm">
+      <div className="flex h-full min-h-screen flex-col font-custom transition-colors duration-300 ease-in-out one:bg-mainBgTheme1 two:bg-mainBgTheme2 three:bg-mainBgTheme3 md:items-center">
+        <div className="flex w-screen flex-1 flex-col justify-center gap-6 text-custom font-bold transition-colors duration-700 ease-in-out ~sm/md:~p-4/8 md:max-w-screen-sm">
           <Header />
           <Output display={output} />
           <Keypad output={output} setOutput={setOutput} />
--- src/components/keypad.jsx
+++ src/components/keypad.jsx
@@ -47,12 +47,12 @@ const Keypad = ({ output, setOutput }) => {
   };
 
   const numClass = [
-    "rounded-lg md:border-b-[6px] border-b-4 one:border-b-grayOrangeKeyShadowTheme1 font-bold one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3 ~sm/md:~px-1/0 ~sm/md:~py-1/0 ~sm/md:~text-custom/5xl",
+    "rounded-lg  border-b-4 one:border-b-grayOrangeKeyShadowTheme1 font-bold one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3 ~sm/md:~px-1/0 ~sm/md:~py-1/0 ~sm/md:~text-custom/5xl",
   ];
 
   return (
     <>
-      <main className="grid w-full grid-cols-4 grid-rows-5 rounded-xl ~sm/md:~gap-3/6 ~sm/md:~px-6/7 ~sm/md:~py-6/8 one:bg-toggleBgTheme1 one:text-whiteTextTheme1 two:bg-toggleBgTheme2 two:text-whiteTextTheme2 three:bg-toggleBgTheme3 three:text-whiteTextTheme3">
+      <main className="grid w-full grid-cols-4 grid-rows-5 rounded-xl ~sm/md:~gap-3/5 ~sm/md:~px-6/7 ~sm/md:~py-6/7 one:bg-toggleBgTheme1 one:text-whiteTextTheme1 two:bg-toggleBgTheme2 two:text-whiteTextTheme2 three:bg-toggleBgTheme3 three:text-whiteTextTheme3">
         <button className={numClass} onClick={() => number(7)}>
           7
         </button>
@@ -63,7 +63,7 @@ const Keypad = ({ output, setOutput }) => {
           9
         </button>
         <button
-          className="rounded-lg border-b-4 uppercase ~sm/md:~text-lg/custom one:border-b-keyShadowTheme1 one:bg-keyBgTheme1 one:hover:bg-hoverKey1 two:border-b-keyShadowTheme2 two:bg-keyBgTheme2 two:hover:bg-hoverKey2 three:border-b-vividMagentaKeyShadowTheme3 three:bg-darkVioletKeyBgTheme3 three:hover:bg-hoverKey3 md:border-b-[6px]"
+          className="rounded-lg border-b-4 uppercase ~sm/md:~text-lg/custom one:border-b-keyShadowTheme1 one:bg-keyBgTheme1 one:hover:bg-hoverKey1 two:border-b-keyShadowTheme2 two:bg-keyBgTheme2 two:hover:bg-hoverKey2 three:border-b-vividMagentaKeyShadowTheme3 three:bg-darkVioletKeyBgTheme3 three:hover:bg-hoverKey3"
           onClick={del}
         >
           Del
@@ -90,13 +90,13 @@ const Keypad = ({ output, setOutput }) => {
           3
         </button>
         <button
-          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3 md:border-b-[6px]"
+          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3"
           onClick={minus}
         >
           -
         </button>
         <button
-          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3 md:border-b-[6px]"
+          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3"
           onClick={dot}
         >
           .
@@ -105,27 +105,27 @@ const Keypad = ({ output, setOutput }) => {
           0
         </button>
         <button
-          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3 md:border-b-[6px]"
+          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3"
           onClick={divide}
         >
           /
         </button>
         <button
-          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3 md:border-b-[6px]"
+          className="rounded-lg border-b-4 ~sm/md:~text-custom/5xl one:border-b-grayOrangeKeyShadowTheme1 one:bg-lightGrayOrangeKeyBgTheme1 one:text-textGrayBlueTheme1 one:hover:bg-hoverNumberKey1 two:border-b-darkGrayOrangeKeyShadowTheme2 two:bg-lightGrayYellowKeyBgTheme2 two:text-textGrayYellowTheme2 two:hover:bg-hoverNumberKey2 three:border-b-darkMagentaKeyShadowTheme3 three:bg-veryDarkVioletKeyBgTheme3 three:text-textLightYellowTheme3 three:hover:bg-hoverNumberKey3"
           onClick={multiply}
         >
           x
         </button>
         <button
           className={
-            "col-span-2 rounded-lg border-b-4 uppercase ~sm/md:~text-lg/custom one:border-b-keyShadowTheme1 one:bg-keyBgTheme1 one:hover:bg-hoverKey1 two:border-b-keyShadowTheme2 two:bg-keyBgTheme2 two:hover:bg-hoverKey2 three:border-b-vividMagentaKeyShadowTheme3 three:bg-darkVioletKeyBgTheme3 three:hover:bg-hoverKey3 md:border-b-[6px]"
+            "three:hover:bg-hoverKey3] col-span-2 rounded-lg border-b-4 uppercase ~sm/md:~text-lg/custom one:border-b-keyShadowTheme1 one:bg-keyBgTheme1 one:hover:bg-hoverKey1 two:border-b-keyShadowTheme2 two:bg-keyBgTheme2 two:hover:bg-hoverKey2 three:border-b-vividMagentaKeyShadowTheme3 three:bg-darkVioletKeyBgTheme3"
           }
           onClick={clear}
         >
           Reset
         </button>
         <button
-          className="col-span-2 rounded-lg border-b-4 ~sm/md:~text-lg/custom one:border-b-darkRedKeyShadowTheme1 one:bg-redKeyBgTheme1 one:hover:bg-hoverEqual1 two:border-b-darkOrangeKeyShadowTheme2 two:bg-orangeKeyBgTheme2 two:hover:bg-hoverEqual2 three:border-b-softCyanKeyShadowTheme3 three:bg-pureCyanKeyBgTheme3 three:text-textVeryDarkBlueTheme3 three:hover:bg-hoverEqual3 md:border-b-[6px]"
+          className="col-span-2 rounded-lg border-b-4 ~sm/md:~text-lg/custom one:border-b-darkRedKeyShadowTheme1 one:bg-redKeyBgTheme1 one:hover:bg-hoverEqual1 two:border-b-darkOrangeKeyShadowTheme2 two:bg-orangeKeyBgTheme2 two:hover:bg-hoverEqual2 three:border-b-softCyanKeyShadowTheme3 three:bg-pureCyanKeyBgTheme3 three:text-textVeryDarkBlueTheme3 three:hover:bg-hoverEqual3"
           onClick={sum}
         >
           =
